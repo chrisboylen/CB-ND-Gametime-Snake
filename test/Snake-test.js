06865cd76613f4d55523835442a70e8e5c49daa1
Chris Boylen <billpolster@gmail.com>
2018-06-27 13:24:18 -0600

    Add test to Snake-test.js to check new instance of Snake

--- test/Snake-test.js
+++ test/Snake-test.js
@@ -1,2 +1,13 @@
 const { expect } = require('chai');
 const Snake = require('../lib/Snake.js');
+
+describe('GamePiece', () => {
+  let snake = new Snake(50, 50, 10, 10, "green", 1, 1.5);
+  
+
+  it('Should exist', () => {
+    expect(snake).to.exist;
+    expect(snake).to.be.an.instanceOf(Snake);
+  });
+
+});
