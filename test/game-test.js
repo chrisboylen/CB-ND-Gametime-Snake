09ce3f620f809d5f6b527e980d6147996f600fbe
Chris Boylen <billpolster@gmail.com>
2018-06-28 18:05:20 -0600

    Add test for instance properties on Game-test

--- test/game-test.js
+++ test/game-test.js
@@ -1,11 +1,14 @@
 const { expect } = require('chai');
 const Game = require('../lib/Game.js');
+const Snake = require('../lib/Snake.js');
+const Food = require('../lib/Food.js');
 
 describe('Game', () => {
   let game;
-
+  let snake;
+  let food;
   beforeEach(() => {
-    game = new Game(context);
+    game = new Game(context, false, 0, 0, snake = new Snake(50, 50, 10, 10, 'rgb(255, 0, 0)', 1), food = new Food(150, 10, 10, 10, 'rgb(0, 255, 0)'));
   });
 
   it('Should exist', () => {
@@ -13,5 +16,17 @@ describe('Game', () => {
     expect(game).to.be.an.instanceOf(Game);
   });
 
-  ir('Should st')
+  it('Should set instance properties', () => {
+    const actualState = game;
+    const expectedState = {
+      context: context,
+      gameOver: false,
+      score: 0,
+      highScore: 0,
+      snake: new Snake(50, 50, 10, 10, 'rgb(255, 0, 0)', 1),
+      food: new Food(150, 10, 10, 10, 'rgb(0, 255, 0)')
+    }
+
+    expect(actualState).to.deep.equal(expectedState);
+  })
 });
