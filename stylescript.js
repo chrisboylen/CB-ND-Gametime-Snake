89c1ee470adb7f7624405d227c71cf92dbc9c93a
Nick Dambrosio <ndambro303@gmail.com>
2018-07-01 13:56:31 -0600

    Add scoreboard-overlay

--- stylescript.js
+++ stylescript.js
@@ -5,3 +5,7 @@ $('.open-instructions').click(function(){
 $('.close-instructions').click(function(){
   $(this).closest('.descriptive-text').find('.descriptive-overlay').animate({left: -500})
 })
+
+$('.view-scoreboard').click(function(){
+  $(this).find('.game-literal').find('.score-overlay').animate({left: -500})
+})
