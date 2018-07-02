9af08734df141cc09a13ab9535a528097493d3bf
Nick Dambrosio <ndambro303@gmail.com>
2018-07-01 19:35:48 -0600

    Fix headings

--- stylescript.js
+++ stylescript.js
@@ -5,7 +5,3 @@ $('.open-instructions').click(function(){
 $('.close-instructions').click(function(){
   $(this).closest('.descriptive-text').find('.descriptive-overlay').animate({left: -500})
 })
-
-$('.view-scoreboard').click(function(){
-  $(this).find('.game-literal').find('.score-overlay').animate({left: -500})
-})
