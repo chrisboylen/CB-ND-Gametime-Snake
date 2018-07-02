81b05fbbd05f720f35a5e5dd284165828f1f5a41
Nick Dambrosio <ndambro303@gmail.com>
2018-07-02 17:47:11 -0600

    Work on collision, fix styling,

--- stylescript.js
+++ stylescript.js
@@ -1,7 +1,12 @@
-$('.open-instructions').click(function(){
-  $(this).closest('.descriptive-text').find('.descriptive-overlay').animate({left: 0})
-})
+$('.open-instructions').click(function() {
+  $(this).closest('.descriptive-text').find('.descriptive-overlay').animate({left: 0});
+});
 
-$('.close-instructions').click(function(){
-  $(this).closest('.descriptive-text').find('.descriptive-overlay').animate({left: -500})
+$('.close-instructions').click(function() {
+  $(this).closest('.descriptive-text').find('.descriptive-overlay').animate({left: -500});
+});
+
+
+$('.new-game').click(function(){
+  $(".game-over").css("visibility", "visible");
 })
