9eac866232d46268dba2a151aff676ea9d939f10
Nick Dambrosio <ndambro303@gmail.com>
2018-07-03 16:09:45 -0600

    Add leaderboard functionality

--- stylescript.js
+++ stylescript.js
@@ -1,25 +1,22 @@
-$('.open-instructions').click(function() {
-  $(this).closest('.descriptive-text').find('.descriptive-overlay').animate({left: 0});
-});
 
-$('.close-instructions').click(function() {
-  $(this).closest('.descriptive-text').find('.descriptive-overlay').animate({left: -500});
-});
 
 
 $('.new-game').click(function(){
   location.reload();
 })
 
-
-
-
-submitNewScore() {
-  let userName = $('.player-score-input'.val();
-  let userScore = $(this.score)
-  let newScore = `<li>${userName} ............. ${userScore}</li>`;
-  $('score-list').prepend(newScore)
+function displayGameOver() {
+  $(".game-over").css("visibility", "visible");
 }
+// $('.submit-score').on('click', submitNewScore);
+
+// const submitNewScore = () => {
+//   let userName = $('.player-score-input'.val());
+//   let userScore = $(this.score)
+//   let newScore = `<li>${userName} ............. ${userScore}</li>`;
+//   $('score-list').prepend(newScore)
+// }
+
 
 
 
