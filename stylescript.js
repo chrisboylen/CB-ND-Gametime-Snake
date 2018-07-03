b1daeb55d58eb038a2c77f2fa11ecff4da663cb0
Nick Dambrosio <ndambro303@gmail.com>
2018-07-03 13:25:17 -0600

    Add gameover functionality

--- stylescript.js
+++ stylescript.js
@@ -11,13 +11,15 @@ $('.new-game').click(function(){
   location.reload();
 })
 
-function displayGameOver() {
-  $(".game-over").css("visibility", "visible");
+
+
+
+submitNewScore() {
+  let userName = $('.player-score-input'.val();
+  let userScore = $(this.score)
+  let newScore = `<li>${userName} ............. ${userScore}</li>`;
+  $('score-list').prepend(newScore)
 }
-
-
-
-
 
 
 
