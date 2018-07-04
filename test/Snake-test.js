d6b72df6428019707f5195515d7e3172ee50fa48
Chris Boylen <billpolster@gmail.com>
2018-07-04 10:26:31 -0600

    Test if lives change when snake dies

--- test/Snake-test.js
+++ test/Snake-test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const Snake = require('../lib/Snake.js');
 
 describe('Snake', () => {
@@ -42,5 +42,37 @@ describe('Snake', () => {
     expect(actualState).toEqual(expectedState);
   })
 
-  // 
+  it('Should be able to change direction to up', () => {
+    snake.changeDirection('up');
+
+    const expectedState = 'up';
+    const actualState = snake.changeDirection('up')
+
+    expect(actualState).equal(expectedState);
+  })
+  
+  it('Should be able to add to snake when eats food', () => {
+    const oldLength = snake.tail.length;
+    const newLength = oldLength.growSnake(this.direction = 'right');
+
+    const expectedState = newLength;
+    const actualState = snake.tail[0] -20;
+
+    expect(actualState).equal(expectedState);
+  })
+
+  it('Should reduce lives when snake dies', () => {
+    const lives = snake.lives;
+    const expectedState = 2;
+    snake.death();
+    const actualState = snake.lives;
+
+    expect(actualState).equal(expectedState);
+  })
+
+  it('Should be able to dies if snake goes out of bounds', () => {
+    
+
+    expect(actualState).equal(expectedState);
+  })
 });
